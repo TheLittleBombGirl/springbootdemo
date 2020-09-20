eef5846949dfee61e1d4bac660b39b2eb3b96d51
thinkpad <yihouyun2020@gmail.com>
2020-09-21 01:21:38 +0800

    添加12471111

--- spring-boot-demo/src/main/java/cn/cy/AquariusApplication.java
+++ spring-boot-demo/src/main/java/cn/cy/AquariusApplication.java
@@ -3,7 +3,7 @@ package cn.cy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//等等
+//等等1247777111
 @SpringBootApplication
 @MapperScan(basePackages = {"cn.cy.*.dao"})
 public class AquariusApplication {
